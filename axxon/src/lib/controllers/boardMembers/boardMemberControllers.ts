118dcce7d510727bfcb018c1c4ea59845610aab1
Xavier Campos <xaviercampos2425@gmail.com>
2025-07-16 02:00:24 -0400

    controller for getting all members in a board has been created

--- axxon/src/lib/controllers/boardMembers/boardMemberControllers.ts
+++ axxon/src/lib/controllers/boardMembers/boardMemberControllers.ts
@@ -1,8 +1,14 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { BoardMembers } from '@/lib/models/boardMembers';
-import { ListBoardsForUser } from '@/lib/models/types/boardMemberTypes';
+import { GetAllMembersForBoard, ListBoardsForUser } from '@/lib/models/types/boardMemberTypes';
 
 export async function listBoardsForUsers (data: ListBoardsForUser) {
     const boards = await BoardMembers.listBoardsForUser(data);
     return NextResponse.json(boards, {status: 200});
 }
+
+export async function getAllMembersInBoard (data: GetAllMembersForBoard) {
+    const members = await BoardMembers.getAllMembersForBoard(data);
+    return NextResponse.json(members, {status: 200});
+}
+

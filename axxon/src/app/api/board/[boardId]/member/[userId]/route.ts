de806b6a378b1aaec4d46721ce93c256232bc5b8
Xavier Campos <xcampos@brooklynsteamcenter.org>
2025-07-21 22:44:19 -0400

    fixed board member controllers and routes to work dynamically within their dedicated routes

--- axxon/src/app/api/board/[boardId]/member/[userId]/route.ts
+++ axxon/src/app/api/board/[boardId]/member/[userId]/route.ts
@@ -1,20 +1,10 @@
 import { getMemberById, removeBoardMember } from '@/lib/controllers/boardMembers/boardMemberControllers';
 import { NextRequest } from 'next/server';
 
-export async function GET(_req: NextRequest, { params }: { params: { boardId: string; userId: string } }) {
-  const data = {
-    board_id: Number(params.boardId),
-    user_id: Number(params.userId),
-  };
-
-  return await getMemberById(data);
+export async function GET(_req: NextRequest, context: {params: {boardId: string; userId: string; }}){
+  return await getMemberById(_req, context.params);
 }
 
-export async function DELETE(_req: NextRequest, { params }: { params: { boardId: string; userId: string } }) {
-  const data = {
-    board_id: Number(params.boardId),
-    user_id: Number(params.userId),
-  };
-
-  return await removeBoardMember(data);
+export async function DELETE(_req: NextRequest, context: {params: {boardId: string; userId: string; }}) {
+  return await removeBoardMember(_req, context.params);
 }
--- axxon/src/lib/controllers/boardMembers/boardMemberControllers.ts
+++ axxon/src/lib/controllers/boardMembers/boardMemberControllers.ts
@@ -1,12 +1,3 @@
-/*
-THIS WHOLE CONTROLLER FILE SHOULD BE REVISED 
-TO ENSURE THAT CONTROLLERS WORK AS INTENDED
-
-controllers that need to be fixed: 
--remove board members
--get by id
-*/
-
 import { NextRequest, NextResponse } from 'next/server';
 import { BoardMembers } from '@/lib/models/boardMembers';
 import { AddBoardMembersByEmail, GetMemberById, RemoveBoardMember } from '@/lib/models/types/boardMemberTypes';
@@ -38,23 +29,48 @@ export async function getAllMembersInBoard(_req: NextRequest, params: {boardId: 
   }
 }
 
-//needs req since its DEL
-export async function removeBoardMember(data: RemoveBoardMember) {
-  const result = await BoardMembers.removeMember(data);
-  return NextResponse.json(result, { status: 200 });
+
+export async function removeBoardMember(_req: NextRequest, params: {boardId: string; userId: string;}) {
+  try{  
+    const board_id = Number(params.boardId);
+    const user_id = Number(params.userId);
+
+    const removal = await BoardMembers.removeMember({user_id, board_id});
+    
+    return NextResponse.json(removal, {status: 200});
+  }catch(error){
+    console.error('[BOARD_MEMBER_REMOVAL_ERROR]', error);
+    return NextResponse.json({error: 'failed to remove board member'}, {status: 500}); 
+  }
 }
 
-//needs req since its a POST
+
 export async function addBoardMembersByEmail(req: NextRequest, params: { boardId: string;}){
-  const data: AddBoardMembersByEmail = await req.json();
-  data.board_id = Number(params.boardId)
-  await BoardMembers.addMembersByEmail(data);
-  return NextResponse.json({ message: 'Members added successfully' }, { status: 200 });
+  try{
+    const data: AddBoardMembersByEmail = await req.json();
+    data.board_id = Number(params.boardId)
+
+    await BoardMembers.addMembersByEmail(data);
+    
+    return NextResponse.json({ message: 'Members added successfully' }, { status: 200 });
+  }catch(error){
+    console.error('[ADD_BOARD_MEMBER_BY_EMAIL_ERROR]', error);
+    return NextResponse.json({error: 'failed to add member by email'}, {status: 500}); 
+  }
 }
 
 //doesnt directly handle http request so it can work with type variable parameter 
 //helper function
-export async function getMemberById(data: GetMemberById) {
-  const member = await BoardMembers.getMemberById(data);
-  return NextResponse.json(member, { status: 200 });
+export async function getMemberById(_req: NextRequest, params: { boardId: string; userId: string; }) {
+  try{ 
+    const board_id = Number(params.boardId);
+    const user_id = Number(params.userId);
+
+    const member = await BoardMembers.getMemberById({board_id, user_id});
+    
+    return NextResponse.json(member, { status: 200 });
+  }catch(error){
+    console.error('[GET_BOARD_MEMBER_BY_ID_ERROR]', error);
+    return NextResponse.json({error: 'failed to get board member by ID'}, {status: 500}); 
+  }
 }
